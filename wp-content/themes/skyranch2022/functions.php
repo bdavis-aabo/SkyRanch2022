38a930aa274a7caf7095f68d875da53409da62b8
bdavis-aabo <bdavis@artandbusinessone.com>
2022-01-20 12:25:13 -0700

    Updating functions.php to remove builder post type and add in the qmi post type and taxonomy.

--- wp-content/themes/skyranch2022/functions.php
+++ wp-content/themes/skyranch2022/functions.php
@@ -30,20 +30,19 @@ if(!function_exists('wpt_setup')):
 endif;
 
 function wpt_register_js(){
-  if( !is_admin()){
+  if(!is_admin()){
     wp_deregister_script('jquery');
 
     wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.3.1.min.js', 'jquery', '', true);
     wp_enqueue_script('jquery.popper.min', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', 'jquery', '', true);
-  	      wp_enqueue_script('jquery.bootstrap.min', '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', 'jquery', '', true);
-  	      wp_enqueue_script(
-      'jquery.extras.min',
+  	wp_enqueue_script('jquery.bootstrap.min', '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', 'jquery', '', true);
+  	wp_enqueue_script(
+    	'jquery.extras.min',
       get_template_directory_uri() . '/assets/js/min/main.min.js',
       array(),
       filemtime(get_template_directory().'/assets/js/min/main.min.js'),
       true
     );
-
   }
 }
 
@@ -61,23 +60,51 @@ function wpt_register_css(){
 add_action('init','wpt_register_js');
 add_action('wp_enqueue_scripts', 'wpt_register_css');
 
-// Custom Post Types
-add_action('init','create_post_type');
-function create_post_type(){
-  register_post_type('home-builders', array(
-	  'label' => __('Home Builders'),
-    'singular_label' => __('Home Builder'),
-    'public' => true,
-    'show_ui' => true,
-    'capability_type' => 'post',
-    'hierarchical' => true,
-    'rewrite' => array('slug' => 'home-builders'),
-    'supports' => array('title','author','excerpt','thumbnail','order','page-attributes','editor'),
-    'menu_position' => 16,
-    'has_archive' => true,
-    'menu_icon' => 'dashicons-admin-home'
+// QUICK MOVE-IN POST TYPE
+add_action('init','create_quick_moves');
+function create_quick_moves(){
+  register_post_type('quickmoves',array(
+    'label'           =>	__('Quick Move-Ins'),
+		'singular_label'	=>	__('Quick Move-In'),
+		'public'          =>	true,
+		'show_ui'         =>	true,
+		'capability_type'	=>	'post',
+		'hierarchical'		=>	'true',
+		'rewrite'         =>	array('slug' => 'quick-moveins'),
+		'supports'        =>	array('title','custom-fields','order','editor','page-attributes'),
+		'menu_position'		=>	22,
+		'menu_icon'       =>	'dashicons-admin-home',
+		'has_archive'     =>	true,
   ));
 }
+// Create Builder Taxonomies
+add_action('init','builder_taxonomies',0);
+function builder_taxonomies(){
+  $_labels = array(
+    'name'              =>	_x('Builders','taxonomy general name'),
+		'singular_name'     =>	_x('Builder', 'taxonomy singular name'),
+		'search_items'		  =>	__('Search Builders'),
+		'all_items'         =>	__('All Builders'),
+		'parent_item'       =>	__('Parent Builder'),
+		'parent_item_colon'	=>	__('Parent Builder:'),
+		'edit_item'         =>	__('Edit Builder'),
+		'update_item'       =>	__('Update Builder'),
+		'add_new_item'      =>	__('Add New Builder'),
+		'new_item_name'     =>	__('New Builder Name'),
+		'menu_name'         =>	__('Builders'),
+  );
+  $_args = array(
+    'hierarchical'      =>	true,
+		'labels'            =>	$_labels,
+		'show_ui'           =>	true,
+		'show_admin_column'	=>	true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'         =>	true,
+		'rewrite'           =>	array('slug' => 'builder'),
+  );
+  register_taxonomy('builder','quickmoves',$_args);
+}
+// end Builder Taxonomies
 
 
 function modify_read_more_link(){
